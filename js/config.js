13028f30fe4c952a08bdb9d66354913fbad404da
Geonovum <>
2023-02-23 12:25:30 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/4add5e0d253ece8569ba19725ded4d1fbfdfcd65

---
+++ js/config.js
@@ -0,0 +1,35 @@
+var respecConfig =
+{
+  pubDomain: "ow",
+  specStatus: "GN-DEF",
+  specType: "HR",
+  license: "cc-by-nd",
+  publishDate: "2023-02-16",
+  content: {
+    "section_000": "",
+    "section_001": "",
+    "section_002": "",
+    "section_003": ""
+  },
+  authors: [
+    {
+      name: "Herbert van Hulten",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Herbert van Hulten",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  shortName: "QGIS",
+  format: "markdown",
+  github: "https://github.com/Geonovum/ow-QGIS/",
+  issueBase: "https://github.com/Geonovum/ow-QGIS/issues/",
+  edDraftURI: "https://Geonovum.github.io/ow-QGIS/",
+  publishISODate: "2023-02-16T00:00:00.000Z",
+  generatedSubtitle: "Vastgestelde versie 16 februari 2023"
+}
